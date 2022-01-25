5faf248b958649f926e75ab2bb841b756222852d
Reymart Narvaez <reymart_narvaez@hotmail.com>
2022-01-25 16:22:49 +0100

    Added methods in EmployeeManagerTest

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -1,5 +1,35 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
 class EmployeeManagerTest {
+
+    @Test
+    void payEmployeesShouldReturnTwo() {
+        EmployeeRepository employeeRepository = new EmployeeRepositoryStub();
+        BankService bankService = new BankServiceStub();
+        EmployeeManager employeeManager = new EmployeeManager(employeeRepository, bankService);
+
+        var result = employeeManager.payEmployees();
+
+        assertEquals(2, result);
+    }
+
+    @Test
+    void payEmployeesShouldThrowRuntimeException() {
+        EmployeeRepository employeeRepository = new EmployeeRepositoryStub();
+        BankService bankService = mock(BankService.class);
+        EmployeeManager employeeManager = new EmployeeManager(employeeRepository, bankService);
+
+        when(employeeManager.payEmployees()).thenThrow(new RuntimeException());
+        var result = employeeManager.payEmployees();
+
+
+        assertEquals(1, result);
+    }
 
 }
--- src/test/java/com/example/EmployeeRepositoryStub.java
+++ src/test/java/com/example/EmployeeRepositoryStub.java
@@ -1,2 +1,16 @@
-package com.example;public class EmployeeRepositoryStub {
+package com.example;
+
+import java.util.List;
+
+public class EmployeeRepositoryStub implements EmployeeRepository {
+    @Override
+    public List<Employee> findAll() {
+        return List.of(new Employee("1", 99.0),
+                new Employee("2", 300.0));
+    }
+
+    @Override
+    public Employee save(Employee e) {
+        return null;
+    }
 }
