95b8e740fc6f12baff43d6d0e73bdf36dcca157e
Reymart Narvaez <reymart_narvaez@hotmail.com>
2022-01-29 13:22:35 +0100

    Added step 1, empty string

---
+++ src/main/java/com/example/StringCalculator.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class StringCalculator {
+    public static int add(String string) {
+        return 0;
+    }
+}
--- src/test/java/com/example/StringCalculatorTest.java
+++ src/test/java/com/example/StringCalculatorTest.java
@@ -1,4 +1,15 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StringCalculatorTest {
+
+    @Test
+    void emptyStringShouldReturnZero(){
+        assertThat(StringCalculator.add("")).isEqualTo(0);
+    }
+
+
 }
